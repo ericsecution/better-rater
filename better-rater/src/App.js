3dd8c2af70a14d633a27910ebac8e25fcde0e9fb
Eric Mangin <ericpaulmangin@gmail.com>
2022-12-06 18:04:47 -0700

    Added shared Folder, adj Components, added deleteFeedback function, etc

--- better-rater/src/App.js
+++ better-rater/src/App.js
@@ -7,12 +7,19 @@ import FeedbackData from './data/FeedbackData'
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
 
+    const deleteFeedback = (id) => {
+        if (window.confirm('Are you sure that you want to delete? (Ok / Cancel)')){
+       setFeedback(feedback.filter((item) => item.id !== id ))
+        }
+    }
+
 
     return (
         <>
         <Header />
         <div className='container'>
-        <FeedbackList feedback={feedback} />
+        <FeedbackList feedback={feedback} 
+        handleDelete={deleteFeedback} />
         </div>
 
         </>
--- better-rater/src/components/FeedbackItem.jsx
+++ better-rater/src/components/FeedbackItem.jsx
@@ -1,13 +1,29 @@
+import { FaTimes } from 'react-icons/fa'
+import PropTypes from 'prop-types'
+import Card from "./shared/Card"
 
-function FeedbackItem({item}) {
+
+function FeedbackItem({ item, handleDelete }) {
+  
 
   return (
-    <div className="card">
+    // <Card reverse={true}> 
+    // use above code to flip cards to dark / reverse colors
+    <Card>
       <div className="num-display">{item.rating}</div>
+      <button onClick={() => handleDelete(item.id)} className='close'>
+        <FaTimes color='purple' />
+      </button>
       <div className="text-display">{item.text}</div>
      
-    </div>
+    </Card>
   )
 
 }
+
+FeedbackItem.propTypes = {
+  item: PropTypes.object.isRequired,
+
+}
+
 export default FeedbackItem
