64faf3d4f9d756203215a775954d6c28315ec81a
Eric Mangin <ericpaulmangin@gmail.com>
2022-12-08 22:14:34 -0700

    Re-adding in spinner.gif; & npm packages (concurrently + json-server), updated package.json, and have added all CRUD capabilities via the useContext hook. App should be all set.

--- better-rater/src/context/FeedbackContext.js
+++ better-rater/src/context/FeedbackContext.js
@@ -1,41 +1,48 @@
-import { v4 as uuidv4 } from 'uuid'
-import { createContext, useState } from 'react'
+import { createContext, useState, useEffect } from 'react'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({children}) => {
-        const [feedback, setFeedback] = useState([
-            {
-                id: 1,
-                text: 'This is FeedbackItem id 1',
-                rating: 10
-            },
-            {
-                id: 2,
-                text: 'This is FeedbackItem id 2',
-                rating: 9
-            },
-            {
-                id: 3,
-                text: 'This is FeedbackItem id 3',
-                rating: 7
-            },
-        ])
+        const [isLoading, setIsLoading] = useState(true)
+        const [feedback, setFeedback] = useState([])
         const [feedbackEdit, setFeedbackEdit] = useState({
             item: {},
             edit: false,
         })
 
+        useEffect(() => {
+            fetchFeedback()
+          }, [])
+        
+          // Fetch feedback
+          const fetchFeedback = async () => {
+            const response = await fetch(`/feedback?_sort=id&_order=desc`)
+            const data = await response.json()
+        
+            setFeedback(data)
+            setIsLoading(false)
+          }
+
         // Create feedback item (with new id, via uuid)
-        const addFeedback = (newFeedback) => {
-            newFeedback.id = uuidv4()
-            setFeedback([newFeedback, ...feedback])
+        const addFeedback = async (newFeedback) => {
+            const response = await fetch('/feedback', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(newFeedback),
+            })
+
+            const data = await response.json()
+
+            setFeedback([data, ...feedback])
         }
 
         // Delete feedback item (incl confirmation pop-up window)
-        const deleteFeedback = (id) => {
+        const deleteFeedback = async (id) => {
             if (window.confirm('Are you sure that you want to delete? (Ok / Cancel)')){
-        setFeedback(feedback.filter((item) => item.id !== id ))
+                await fetch(`/feedback/${id}`, { method: 'DELETE' })
+                setFeedback(feedback.filter((item) => item.id !== id ))
             }
         }
 
@@ -48,8 +55,18 @@ export const FeedbackProvider = ({children}) => {
         }
 
         // actually Update the feedback item
-        const updateFeedback = (id, updItem) => {
-            setFeedback(feedback.map((item) => item.id === id ? {...item, ...updItem } : item))
+        const updateFeedback = async (id, updItem) => {
+            const response = await fetch(`/feedback/${id}`, { 
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(updItem) 
+            })
+
+            const data = await response.json()
+
+            setFeedback(feedback.map((item) => item.id === id ? {...item, ...data } : item))
         }
 
     return (
@@ -60,6 +77,7 @@ export const FeedbackProvider = ({children}) => {
             value={{
                 feedback,
                 feedbackEdit,
+                isLoading,
                 deleteFeedback,
                 addFeedback,
                 editFeedback,
